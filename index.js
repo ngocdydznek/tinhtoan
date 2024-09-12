a8b01aeecd3b9eb9eac4eae0bda34d7fe00f72a5
ngocdydznek <168576578+ngocdydznek@users.noreply.github.com>
2024-09-12 19:44:24 +0700

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const math = require('mathjs');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bot is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 const client = new Client({
     intents: [
